4216d0b8b48bd559af72a6bccababc21198e90bf
Andrew Ndhlovu <drewxdvst@outlook.com>
2020-07-22 23:51:16 +0200

    fix issue with region failing coverage threshold

--- bin/get_region.py
+++ bin/get_region.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import argparse
 import pprint
 import csv
+import sys
 
 __author__ = "Andrew Ndhlovu"
 __copyright__ = "Copyright 2020"
@@ -19,19 +20,35 @@ class GetRegion(object):
 
     """ A  parser for popoolation2 *_rc file   """
 
-    def __init__(self, rc_fp):
+    def __init__(self, rc_fp, target_coverage):
         
         """
             Gets the file object for reading by the argparse
+            
+        """
+
+
+        self.target_coverage = target_coverage
+        comment  =  next(rc_fp)
+        assert comment.startswith("##"), "First line of the *_rc files must be a comment"
+        
+        self.csv_reader = csv.reader(rc_fp, delimiter='\t')
+        
+    def get(self):
+        
+        """
+           get region from the file
 
         """
-    
-        csv_reader = csv.reader(rc_fp, delimiter='\t')
-        _  =  next(csv_reader)
         
         region_dict = OrderedDict() 
-        for row in csv_reader:
+        for row in self.csv_reader:
             region = row[0]
+            #get major allels and corresponding freq
+            if not self.check_target(zip(row[7],row[9:])):
+                sys.stdout.write("!fail "+"\t".join(row) + "\n")
+                continue
+            
             region_dict.setdefault(region,[]).append(int(row[1]))
 
         #pprint.pprint(region_dict)    
@@ -41,13 +58,25 @@ class GetRegion(object):
             min_x, max_x = min_max(values)
             #To stdout
             print("{}:{}-{}".format(key, min_x, max_x))
+
+            
+    def check_target(self, major_freqs):
+        get_coverage = lambda frac : map(int, frac.split('/'))
+        for allele_rec in major_freqs:
+            count, cov = get_coverage(allele_rec[1])
+            if cov <= self.target_coverage:
+                return False
+            
+        return True
             
             
 if  __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A  parser for popoolation *_rc file")
     parser.add_argument('rc_file', type=argparse.FileType('r'))
+    parser.add_argument('-t','--target_coverage', type=int, default = 30)
     args = parser.parse_args()
-    GetRegion(args.rc_file)
+    region = GetRegion(args.rc_file, args.target_coverage)
+    region.get()
 
 
 
